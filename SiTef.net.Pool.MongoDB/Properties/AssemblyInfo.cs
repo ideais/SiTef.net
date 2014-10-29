1e9bdc4a77f01b324081eb07149e3cbefd52d245
Victor Hogemann <victor.hogemann@ideais.com.br>
2014-10-29 16:31:05 -0200

    Release 0.0.0.12

--- SiTef.net.Pool.MongoDB/Properties/AssemblyInfo.cs
+++ SiTef.net.Pool.MongoDB/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SiTef.net.Pool.MongoDB")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Implementação de repositório para o Pool de Terminais utilizando MongoDB")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("B2W Digital")]
 [assembly: AssemblyProduct("SiTef.net.Pool.MongoDB")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright B2W Digital ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.12")]
+[assembly: AssemblyFileVersion("0.0.0.12")]
