7b04b6531d4512e1df15cda058314a2b51d6ce8a
Rodrigo Graúdo <rodrigo.graudo@ingresso.com>
2016-09-12 16:50:45 -0300

    Criação de pool dinamico para SiTef.

---
+++ SiTef.net.Pool.Dynamic.Tests/DynamicTerminalRepositoryTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SiTef.net.Pool;
+using SiTef.net.Pool.Dynamic;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace SiTef.net.Pool.Dynamic.Tests
+{
+    [TestClass]
+    public class DynamicTerminalRepositoryTests
+    {
+        static DynamicTerminalRepository dynamicTerminalRepository;
+
+        [ClassInitialize]
+        public static void Init(TestContext context)
+        {
+            dynamicTerminalRepository = new DynamicTerminalRepository();
+        }
+
+        [TestMethod]
+        public async Task TestLease()
+        {
+            var terms = new List<string>();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                var term = await dynamicTerminalRepository.LeaseAsync(string.Empty);
+                bool test = terms.Contains(term.Terminal);
+                Assert.IsFalse(test, string.Format("{0} quantidade", terms.Count));
+                terms.Add(term.Terminal);
+            }
+        }
+    }
+}
---
+++ SiTef.net.Pool.Dynamic/DynamicTerminalRepository.cs
@@ -0,0 +1,45 @@
+﻿using SiTef.net.Pool.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SiTef.net.Pool.Dynamic
+{
+    public class DynamicTerminalRepository : ITerminalRepository
+    {
+        public Task<TerminalLease> LeaseAsync(string id)
+        {
+            //SiTef format AA999999
+
+            var letterRandom = new Random(Guid.NewGuid().GetHashCode());
+            var position1 = (char)letterRandom.Next(65, 90);
+            var position2 = (char)letterRandom.Next(65, 90);
+
+            var numberRandom = new Random(Guid.NewGuid().GetHashCode());
+
+            int number;
+
+            do
+            {
+                number = numberRandom.Next(1, 999999);
+            }
+            while (number >= 900 && number <= 999);
+
+            var terminal = new TerminalLease();
+            terminal.Terminal = string.Format("{0}{1}{2}", position1, position2, number.ToString().PadLeft(6, '0'));
+
+            return Task.FromResult(terminal);
+        }
+
+        public Task<List<TerminalLease>> ReclaimAsync(string id)
+        {
+            return Task.FromResult(new List<TerminalLease>());
+        }
+
+        public Task ReleaseAsync(string terminal)
+        {
+            // Ignora o Release do Repository
+            return Task.FromResult(0);
+        }
+    }
+}
